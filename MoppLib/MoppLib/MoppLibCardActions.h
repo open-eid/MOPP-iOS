21950ce75e1a7b3a376cbd268de48256076b97ff
Katrin Annuk <katrin.annuk@lab.mobi>
2017-01-16 13:09:06 +0200

    Added documentation for public Mopp lib methods

--- MoppLib/MoppLib/MoppLibCardActions.h
+++ MoppLib/MoppLib/MoppLibCardActions.h
@@ -16,17 +16,61 @@
 
 @interface MoppLibCardActions : NSObject
 
-/**
- *Reads personal data file from card
- */
+/** Gets public personal data from ID card.
+*
+* @param controller    ViewController to be used for card reader selection if needed.
+* @param success       Block to be called on successful completion of action. Includes card owner public personal data as MoppLibPersonalData.
+* @param failure       Block to be called when action fails. Includes error.
+*/
 + (void)cardPersonalDataWithViewController:(UIViewController *)controller success:(void(^)(MoppLibPersonalData *))success failure:(void(^)(NSError *))failure;
 
+/**
+ * Checks if reader is connected to device.
+ *
+ * @return YES if reader is connected, NO otherwise
+ */
 + (BOOL)isReaderConnected;
+
+/**
+ * Checks if card is inserted to card reader.
+ *
+ * @param completion    Block to be called when action is complete. Includes BOOL to represent card status - YES if card is detected in reader, NO if card is not found.
+ */
 + (void)isCardInserted:(void(^)(BOOL)) completion;
 
+/**
+ * Gets signing certificate data.
+ *
+ * @param controller    ViewController to be used for card reader selection if needed.
+ * @param success       Block to be called on successful completion of action. Includes signing certificate data as MoppLibCertData
+ * @param failure       Block to be called when action fails. Includes error.
+ */
 + (void)signingCertWithViewController:(UIViewController *)controller success:(void (^)(MoppLibCertData *))success failure:(void (^)(NSError *))failure;
+
+/**
+ * Gets authentication certificate data.
+ *
+ * @param controller    ViewController to be used for card reader selection if needed.
+ * @param success       Block to be called on successful completion of action. Includes authentication certificate data as MoppLibCertData
+ * @param failure       Block to be called when action fails. Includes error.
+ */
 + (void)authenticationCertWithViewController:(UIViewController *)controller success:(void (^)(MoppLibCertData *))success failure:(void (^)(NSError *))failure;
 
+/**
+ * Gets PIN1 retry counter value.
+ *
+ * @param controller    ViewController to be used for card reader selection if needed.
+ * @param success       Block to be called on successful completion of action. Includes retry counter value as NSNumber
+ * @param failure       Block to be called when action fails. Includes error.
+ */
 + (void)pin1RetryCountWithViewController:(UIViewController *)controller success:(void (^)(NSNumber *))success failure:(void (^)(NSError *))failure;
+
+/**
+ * Gets PIN2 retry counter value.
+ *
+ * @param controller    ViewController to be used for card reader selection if needed.
+ * @param success       Block to be called on successful completion of action. Includes retry counter value as NSNumber
+ * @param failure       Block to be called when action fails. Includes error.
+ */
 + (void)pin2RetryCountWithViewController:(UIViewController *)controller success:(void (^)(NSNumber *))success failure:(void (^)(NSError *))failure;
 @end
--- MoppLib/MoppLib/MoppLibPinActions.h
+++ MoppLib/MoppLib/MoppLibPinActions.h
@@ -11,12 +11,70 @@
 
 @interface MoppLibPinActions : NSObject
 
+/**
+ * Changes current PIN1 to new one by using current PIN1 for verification.
+ *
+ * @param newPin1       New PIN1 that will replace current PIN1
+ * @param oldPin1       Current PIN1 to be used for verification
+ * @param controller    ViewController to be used for card reader selection if needed
+ * @param success       Block to be called on successful completion of action
+ * @param failure       Block to be called when action fails. Includes error.
+ */
 + (void)changePin1To:(NSString *)newPin1 withOldPin1:(NSString *)oldPin1 viewController:(UIViewController *)controller success:(void(^)(void))success failure:(void(^)(NSError *))failure;
+
+/**
+ * Changes current PIN1 to new one by using current PUK for verification.
+ *
+ * @param newPin1       New PIN1 that will replace current PIN1.
+ * @param puk           Current PUK to be used for verification.
+ * @param controller    ViewController to be used for card reader selection if needed.
+ * @param success       Block to be called on successful completion of action.
+ * @param failure       Block to be called when action fails. Includes error.
+ */
 + (void)changePin1To:(NSString *)newPin1 withPuk:(NSString *)puk viewController:(UIViewController *)controller success:(void(^)(void))success failure:(void(^)(NSError *))failure;
+
+/**
+ * Changes current PIN2 to new one by using current PIN2 for verification.
+ *
+ * @param newPin2       New PI21 that will replace current PIN2.
+ * @param oldPin2       Current PIN2 to be used for verification.
+ * @param controller    ViewController to be used for card reader selection if needed.
+ * @param success       Block to be called on successful completion of action.
+ * @param failure       Block to be called when action fails. Includes error.
+ */
 + (void)changePin2To:(NSString *)newPin2 withOldPin2:(NSString *)oldPin2 viewController:(UIViewController *)controller success:(void(^)(void))success failure:(void(^)(NSError *))failure;
+
+/**
+ * Changes current PIN2 to new one by using current PUK for verification.
+ *
+ * @param newPin2       New PIN2 that will replace current PIN2.
+ * @param puk           Current PUK to be used for verification.
+ * @param controller    ViewController to be used for card reader selection if needed.
+ * @param success       Block to be called on successful completion of action.
+ * @param failure       Block to be called when action fails. Includes error.
+ */
 + (void)changePin2To:(NSString *)newPin2 withPuk:(NSString *)puk viewController:(UIViewController *)controller success:(void(^)(void))success failure:(void(^)(NSError *))failure;
 
+/**
+ * Unblocks PIN1 by changing current PIN1.
+ *
+ * @param puk           Current PUK to be used for verification.
+ * @param newPin1       New PIN1 that will replace current PIN1.
+ * @param controller    ViewController to be used for card reader selection if needed.
+ * @param success       Block to be called on successful completion of action.
+ * @param failure       Block to be called when action fails. Includes error.
+ */
 + (void)unblockPin1WithPuk:(NSString *)puk newPin1:(NSString *)newPin1 viewController:(UIViewController *)controller success:(void(^)(void))success failure:(void(^)(NSError *))failure;
+
+/**
+ * Unblocks PIN2 by changing current PIN2.
+ *
+ * @param puk           Current PUK to be used for verification.
+ * @param newPin2       New PIN2 that will replace current PIN2.
+ * @param controller    ViewController to be used for card reader selection if needed.
+ * @param success       Block to be called on successful completion of action.
+ * @param failure       Block to be called when action fails. Includes error.
+ */
 + (void)unblockPin2WithPuk:(NSString *)puk newPin2:(NSString *)newPin2 viewController:(UIViewController *)controller success:(void(^)(void))success failure:(void(^)(NSError *))failure;
 
 @end
