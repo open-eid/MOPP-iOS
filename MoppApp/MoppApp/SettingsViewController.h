0cc70793d0448043e00859af257c9aa040cd5038
Olev Abel <olev.abel@lab.mobi>
2017-01-25 09:58:27 +0200

    Settings now have lib version string

--- MoppApp/MoppApp/SettingsViewController.h
+++ MoppApp/MoppApp/SettingsViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SettingsTableViewController : UITableViewController
+@interface SettingsViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 
 @end
