7cee4f1e78f6717901b41e8e5d13e972024fe494
Ants Käär <ants.kaar@lab.mobi>
2017-02-28 10:44:16 +0200

    Hotfix for second mID signature addition

--- MoppApp/MoppApp/ContainerDetailsViewController.m
+++ MoppApp/MoppApp/ContainerDetailsViewController.m
@@ -170,7 +170,7 @@ typedef enum : NSUInteger {
     if (![[DefaultsHelper getIDCode] isEqualToString:idCodeTextField.text] || ![[DefaultsHelper getPhoneNumber] isEqualToString:phoneNumberTextField.text]) {
       [weakSelf askToPersistMobileIDCredentialsWithIdCode:idCodeTextField.text andPhoneNumber:phoneNumberWithCountryCode];
     }else if ([self setConsitsOfIdCode:idCodeTextField.text]) {
-      [weakSelf showSignatureAlreadyExistsWarningAlertWithIDCode:idCodeTextField.text andPhoneNumber:phoneNumberTextField.text];
+      [weakSelf showSignatureAlreadyExistsWarningAlertWithIDCode:idCodeTextField.text andPhoneNumber:phoneNumberWithCountryCode];
     }else {
       [weakSelf mobileCreateSignatureWithIDCode:idCodeTextField.text phoneNumber:phoneNumberWithCountryCode];
     }
