30cc273b4db78b4e3f85bb5b530fc0d7942af571
Marten Rebane <marten.rebane@nortal.com>
2019-10-08 14:45:15 +0300

    Personal code field has no max character count
    * MOPPIOS-621 - Personal code field has no max character count
    
    Signed-off-by: Marten Rebane marten.rebane@nortal.com

--- MoppApp/MoppApp/SettingsViewController.swift
+++ MoppApp/MoppApp/SettingsViewController.swift
@@ -134,6 +134,9 @@ extension SettingsViewController: UITableViewDataSource {
                 let fieldCell = tableView.dequeueReusableCell(withType: SettingsFieldCell.self, for: indexPath)!
                     fieldCell.delegate = self
                     fieldCell.populate(with: field)
+                if (field.id == .personalCode) {
+                    fieldCell.textField.addTarget(self, action: #selector(editingChanged(sender:)), for: .editingChanged)
+                }
                 return fieldCell
             case .timestamp:
                 let timeStampCell = tableView.dequeueReusableCell(withType: SettingsTimeStampCell.self, for: indexPath)!
@@ -148,6 +151,13 @@ extension SettingsViewController: UITableViewDataSource {
                 let groupSeparatorCell = tableView.dequeueReusableCell(withIdentifier: "SettingsGroupSeparator", for: indexPath)
                 return groupSeparatorCell
             }
+        }
+    }
+    
+    @objc func editingChanged(sender: UITextField) {
+        let text = sender.text ?? String()
+        if (text.count > 11) {
+            sender.deleteBackward()
         }
     }
 }
