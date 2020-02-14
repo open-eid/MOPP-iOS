8247603b222476e539e7eb5b1aac5a99ce9c4172
Marten Rebane <marten.rebane@nortal.com>
2020-02-14 12:26:36 +0200

    Initial SK Signing Lib framework setup
    * MOPPIOS-620
    
    Signed-off-by: Marten Rebane marten.rebane@nortal.com

---
+++ SkSigningLib/SkSigningLib/SkSigningLib.h
@@ -0,0 +1,19 @@
+//
+//  SkSigningLib.h
+//  SkSigningLib
+//
+//  Created by Marten Rebane on 14.02.2020.
+//  Copyright © 2020 Riigi Infosüsteemi Amet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SkSigningLib.
+FOUNDATION_EXPORT double SkSigningLibVersionNumber;
+
+//! Project version string for SkSigningLib.
+FOUNDATION_EXPORT const unsigned char SkSigningLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SkSigningLib/PublicHeader.h>
+
+
