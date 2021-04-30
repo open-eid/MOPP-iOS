54cc88d0b458ae2b1385f4f84fffa30054dff1cc
Marten Rebane <54431068+martenrebane@users.noreply.github.com>
2021-04-30 11:06:19 +0300

    Copyright updates (#187)
    * MOPPIOS-861
    
    Signed-off-by: Marten Rebane marten.rebane@nortal.com

--- CryptoLib/CryptoLib/Ldap/ResultSet.h
+++ CryptoLib/CryptoLib/Ldap/ResultSet.h
@@ -2,7 +2,7 @@
 //  ResultSet.h
 //  CryptoLib
 /*
- * Copyright 2017 Riigi Infosüsteemide Amet
+ * Copyright 2017 - 2021 Riigi Infosüsteemi Amet
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
--- SkSigningLib/SkSigningLib/Models/SigningError.swift
+++ SkSigningLib/SkSigningLib/Models/SigningError.swift
@@ -3,7 +3,7 @@
 //  SkSigningLib
 //
 /*
- * Copyright 2020 Riigi Infosüsteemide Amet
+ * Copyright 2017 - 2021 Riigi Infosüsteemi Amet
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
