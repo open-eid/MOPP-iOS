249cbcb30ea1bf0de6a3202c8edd7b85dd56b78c
Marten Rebane <54431068+martenrebane@users.noreply.github.com>
2021-06-21 17:32:49 +0300

    DDOC status changes (#213)
    * MOPPIOS-858
    
    Signed-off-by: Marten Rebane marten.rebane@nortal.com

--- MoppApp/MoppApp/DefaultsHelper.swift
+++ MoppApp/MoppApp/DefaultsHelper.swift
@@ -50,6 +50,7 @@ fileprivate let kCrashReportSettingKey = "kCrashReportSettingKey"
 fileprivate let kPreviousPreferredLanguage = "kPreviousPreferredLanguage"
 fileprivate let kMoppLanguage = "kMoppLanguage"
 fileprivate let kHideShareContainerDialog = "kHideShareContainerDialog"
+fileprivate let kIsTimestampedDdoc = "kIsTimestampedDdoc"
 
 class DefaultsHelper
 {
@@ -149,6 +150,15 @@ class DefaultsHelper
         }
         get {
             return UserDefaults.standard.bool(forKey: kHideShareContainerDialog)
+        }
+    }
+    
+    class var isTimestampedDdoc: Bool {
+        set {
+            UserDefaults.standard.set(newValue, forKey: kIsTimestampedDdoc)
+        }
+        get {
+            return UserDefaults.standard.bool(forKey: kIsTimestampedDdoc)
         }
     }
 
