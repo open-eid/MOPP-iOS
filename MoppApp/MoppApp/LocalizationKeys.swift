f345d41a334ebe26b0270c9025915da8d369f872
Marten Rebane <54431068+martenrebane@users.noreply.github.com>
2021-06-29 09:48:12 +0300

    Removed test signature type (#217)
    * MOPPIOS-831
    
    Signed-off-by: Marten Rebane marten.rebane@nortal.com

--- MoppApp/MoppApp/LocalizationKeys.swift
+++ MoppApp/MoppApp/LocalizationKeys.swift
@@ -97,7 +97,6 @@ enum LocKey : String
     case containerSignatureStatusNonQscd = "container-signature-status-non-qscd";
     case containerSignatureStatusInvalid = "container-signature-status-invalid";
     case containerSignatureStatusUnknown = "container-signature-status-unknown";
-    case conatinerSignatureTestSignatureTitle = "container-signature-test-signature-title";
     case containerTimestampInvalid = "container-timestamp-invalid";
     case challengeCodeLabel = "challenge-code-label"
     case challengeCodeLabelAccessibility = "challenge-code-label-accessibility"
