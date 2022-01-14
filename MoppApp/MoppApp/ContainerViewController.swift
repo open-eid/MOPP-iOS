b250504b888cb56267ea027e8dd87bf4a136bb14
Marten Rebane <54431068+martenrebane@users.noreply.github.com>
2022-01-14 19:52:27 +0200

    iOS 15 file adding fixes

--- MoppApp/MoppApp/ContainerViewController.swift
+++ MoppApp/MoppApp/ContainerViewController.swift
@@ -68,6 +68,11 @@ class ContainerViewController : MoppViewController, ContainerActions, PreviewAct
     var isAsicContainer = LandingViewController.shared.containerType == .asic
     var isEmptyFileWarningSet = false
     @IBOutlet weak var tableView: UITableView!
+    
+    var isSignaturesEmpty: Bool {
+        if !isAsicContainer { return true }
+        return signingContainerViewDelegate.getSignaturesCount() == 0
+    }
 
     enum Section {
         case notifications
@@ -272,6 +277,20 @@ class ContainerViewController : MoppViewController, ContainerActions, PreviewAct
         }
     }
     
+    func setSections() {
+        if isSignaturesEmpty {
+            sections = (isForPreview || !isCreated) ? ContainerViewController.sectionsDefault : ContainerViewController.sectionsNoSignatures
+            if let signaturesIndex = sections.firstIndex(where: { $0 == .signatures }) {
+                if !sections.contains(.missingSignatures) {
+                    sections.insert(.missingSignatures, at: signaturesIndex + 1)
+                }
+            }
+        }
+        else {
+            sections = ContainerViewController.sectionsDefault
+        }
+    }
+    
 }
 
 extension ContainerViewController : LandingViewControllerTabButtonsDelegate {
@@ -338,9 +357,7 @@ extension ContainerViewController : UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if signingContainerViewDelegate.getSignaturesCount() == 0 && sections.contains(.signatures) {
-            reloadContainer()
-        }
+        setSections()
         let row = indexPath.row
         switch sections[indexPath.section] {
         case .notifications:
@@ -674,10 +691,6 @@ extension ContainerViewController : UITableViewDelegate {
         if containerViewDelegate.isContainerEmpty() {
             return
         }
-        var isSignaturesEmpty: Bool {
-            if !isAsicContainer { return true }
-            return signingContainerViewDelegate.getSignaturesCount() == 0
-        }
         if isForPreview {
             updateState(.preview)
         }
@@ -688,18 +701,7 @@ extension ContainerViewController : UITableViewDelegate {
             updateState(.opened)
         }
         if isAsicContainer {
-            if isSignaturesEmpty {
-                    sections = (isForPreview || !isCreated) ? ContainerViewController.sectionsDefault : ContainerViewController.sectionsNoSignatures
-                if let signaturesIndex = sections.firstIndex(where: { $0 == .signatures }) {
-                    if !sections.contains(.missingSignatures) {
-                        sections.insert(.missingSignatures, at: signaturesIndex + 1)
-                    }
-                }
-            }
-            else {
-                sections = ContainerViewController.sectionsDefault
-            }
-        
+            setSections()
         }
         
         tableView.reloadData()
