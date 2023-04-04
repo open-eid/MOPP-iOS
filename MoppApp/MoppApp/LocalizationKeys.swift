f930aa7ed54e463279313f9c9dc38ec6774ec119
Marten Rebane <54431068+martenrebane@users.noreply.github.com>
2023-04-04 15:56:05 +0300

    Improved crypto view personal code handling

--- MoppApp/MoppApp/LocalizationKeys.swift
+++ MoppApp/MoppApp/LocalizationKeys.swift
@@ -335,7 +335,8 @@ enum LocKey : String
     case cryptoAddAddresseeButtonTitle = "crypto-add-addressee-button-title";
     case cryptoAddAddresseeButtonTitleAccessibility = "crypto-add-addressee-button-title-accessibility";
     case cryptoAddresseeAddedButtonTitle = "crypto-addressee-added-button-title";
-    case cryptoRemoveAddresseeButton = "crypto-remove-addressee-button"
+    case cryptoRemoveAddresseeButton = "crypto-remove-addressee-button";
+    case cryptoInvalidPersonalCodeTitle = "crypto-invalid-personal-code-title";
     case datafilePreviewFailed = "datafile-preview-failed";
 
     case digidocImageAccessibility = "digidoc-image-accessibility";
