f6b46acb0591486cc3d89eba99260578d7d41e9e
Boriss Melikjan <boriss.melikjan@nortal.com>
2023-12-05 16:20:07 +0200

    MOPPIOS-1265: Test crash button, revert me after QA testing.

--- MoppApp/MoppApp/DiagnosticsViewController.swift
+++ MoppApp/MoppApp/DiagnosticsViewController.swift
@@ -174,6 +174,16 @@ class DiagnosticsViewController: MoppViewController, UIDocumentPickerDelegate {
         configurationToUI()
 
         listenForConfigUpdates()
+        let button = UIButton(type: .roundedRect)
+        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+        button.setTitle("Test Crash", for: [])
+        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
+    }
+
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+        let numbers = [0]
+        let _ = numbers[1]
     }
 
     // If user clicks on "Additional information", set the switch back to "Off" state
