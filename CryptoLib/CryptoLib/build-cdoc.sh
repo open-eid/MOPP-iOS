19634ea4e63e09cac651a51ab07a2d13c2c0871c
Raul Metsma <raul@metsma.ee>
2024-01-10 23:11:26 +0200

    Fix appstore build
    MOPPIOS-1255
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- CryptoLib/CryptoLib/build-cdoc.sh
+++ CryptoLib/CryptoLib/build-cdoc.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-if [ ! -d cdoc ]; then
-    git clone -b mopp https://github.com/metsma/cdoc.git;
-fi
 export PATH=$PATH:/usr/local/bin:/opt/homebrew/bin
-BUILD_DIR=${TARGET_BUILD_DIR}/cdoc
+SOURCE_DIR=${DERIVED_SOURCES_DIR}/cdoc
+if [ ! -d ${SOURCE_DIR} ]; then
+    git clone -b mopp https://github.com/metsma/cdoc.git ${SOURCE_DIR};
+fi
 cmake \
     -DINSTALL_FRAMEWORKDIR=${BUILT_PRODUCTS_DIR} \
     -DCMAKE_INSTALL_PREFIX=${BUILT_PRODUCTS_DIR} \
     -DCMAKE_BUILD_TYPE=${CONFIGURATION} \
     -DCMAKE_OSX_SYSROOT=${PLATFORM_NAME} \
     -DCMAKE_OSX_ARCHITECTURES="${ARCHS// /;}" \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=${IPHONEOS_DEPLOYMENT_TARGET} \
     -DBUILD_SHARED_LIBS=YES \
     -DOPENSSL_ROOT_DIR=${PROJECT_DIR}/../MoppLib/MoppLib/libdigidocpp/libdigidocpp.${PLATFORM_NAME} \
     -DSWIG_DIR=NOTFOUND \
     -DDOXYGEN_EXECUTABLE=NOTFOUND \
-    -S cdoc -B ${BUILD_DIR}
-cmake --build ${BUILD_DIR}
-cmake --install ${BUILD_DIR}
+    -S ${SOURCE_DIR} -B ${TARGET_TEMP_DIR}
+cmake --build ${TARGET_TEMP_DIR}
+cmake --install ${TARGET_TEMP_DIR}
