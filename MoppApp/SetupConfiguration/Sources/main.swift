a1d6da1b46c8c0bb18b15779b61825fb5fb240c8
Marten Rebane <54431068+martenrebane@users.noreply.github.com>
2025-02-25 05:42:20 +0200

    Update main.swift

--- MoppApp/SetupConfiguration/Sources/main.swift
+++ MoppApp/SetupConfiguration/Sources/main.swift
@@ -220,8 +220,8 @@ class SettingsConfiguration: NSObject, URLSessionDelegate, URLSessionTaskDelegat
     
     /* Path and file saving / moving */
     private func getCurrentPath() -> String {
-        print("Getting current path: ")
-        print(FileManager.default.currentDirectoryPath)
+        NSLog("Getting current path: ")
+        NSLog(FileManager.default.currentDirectoryPath)
         return FileManager.default.currentDirectoryPath
     }
     
@@ -250,9 +250,9 @@ class SettingsConfiguration: NSObject, URLSessionDelegate, URLSessionTaskDelegat
     func moveFile(fileAtPath: URL, fileNameWithExtension: String) {
         let destinationDirectory: URL = URL(string: getCurrentPath())!.deletingLastPathComponent().appendingPathComponent("MoppApp").appendingPathComponent("MoppApp").appendingPathComponent(fileNameWithExtension)
 
-        print("Current path: \(getCurrentPath())")
-        print("Destination path: \(destinationDirectory.path)")
-        print("FileAtPath: \(fileAtPath.path)")
+        NSLog("Current path: \(getCurrentPath())")
+        NSLog("Destination path: \(destinationDirectory.path)")
+        NSLog("FileAtPath: \(fileAtPath.path)")
 
         do {
             if FileManager.default.fileExists(atPath: destinationDirectory.path) {
